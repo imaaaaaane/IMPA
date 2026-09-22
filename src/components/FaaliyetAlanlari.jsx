4e68fc513d54c114d9ef5fe68e868270c18a926f
Emy <Emy@Serhat-MacBook-Pro.local>
2026-09-22 20:30:21 +0300

    fix(ui): revert fabric.webm to use local path

--- src/components/FaaliyetAlanlari.jsx
+++ src/components/FaaliyetAlanlari.jsx
@@ -56,7 +56,7 @@ export default function FaaliyetAlanlari() {
             preload="auto"
             className="absolute inset-0 w-full h-full object-cover z-0"
           >
-            <source src={getImageUrl('/fabric.webm')} type="video/webm" />
+            <source src="/fabric.webm" type="video/webm" />
           </video>
         </div>
 
